e4ea92a7a576cae1041b09689bb7403e32c0b8e6
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-04-24 17:10:24 +0530

    refactoring 1: conditionally rendering login form

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,42 @@ import { Daypicker } from "./calender/Daypicker";
 import { Toaster } from "react-hot-toast";
 import YearlyExpenseTracker from "./expenses/YearlyExpenseTracker";
 import Login from "./auth-components/Login";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    async function loginUser() {
+      try {
+        const user = await axios.get(
+          "http://127.0.0.1:3000/api/users/getUser",
+          {
+            withCredentials: true,
+          }
+        );
+        setUser(user.data.user);
+      } catch (error) {
+        console.error("Error logging in:", error);
+        setUser(null);
+      }
+      console.log(user);
+    }
+    loginUser();
+  });
+
   return (
-    <>
-      <Toaster />
-      <Daypicker />
-      <YearlyExpenseTracker />
-      <Login />
-    </>
+    <div>
+      {user ? (
+        <>
+          <Toaster />
+          <Daypicker />
+          <YearlyExpenseTracker />
+        </>
+      ) : (
+        <Login />
+      )}
+    </div>
   );
 }
 
